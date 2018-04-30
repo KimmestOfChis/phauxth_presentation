692b185ef159b247e6b2853a889394dfd061e960
Matthew <memjay3279@gmail.com>
2018-04-29 22:33:05 -0400

    Tests

--- test/may_second_web/controllers/page_controller_test.exs
+++ test/may_second_web/controllers/page_controller_test.exs
@@ -1,6 +1,7 @@
 defmodule MaySecondWeb.PageControllerTest do
   use MaySecondWeb.ConnCase
 
+ 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
     assert html_response(conn, 200) =~ "Authentication with Phauxth!"
